fafba05ab067e56c210114af53f0477e85b8cf6f
TheSlaylord <62336673+TheSlaylord@users.noreply.github.com>
2020-06-13 14:43:15 +0300

    Create goldCrusher.js

---
+++ scripts/goldCrusher.js
@@ -0,0 +1,12 @@
+const customPulverizer = extendContent(GenericCrafter, "goldCrusher", {
+    draw(tile){
+        Draw.rect(Core.atlas.find(this.name), tile.drawx(), tile.drawy());
+        Draw.rect(Core.atlas.find(this.name + "-rotator"), tile.drawx(), tile.drawy(), tile.entity.totalProgress * 2);
+    },
+    generateIcons: function(){
+        return [
+            Core.atlas.find(this.name)
+        ];
+    },    
+});
+ 
