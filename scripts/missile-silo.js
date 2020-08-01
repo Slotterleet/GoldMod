1e36fd1d9064634244e25960d0d8866ebf6863b3
TheSlaylord <62336673+TheSlaylord@users.noreply.github.com>
2020-08-01 16:15:14 +0300

    update missile-silo.js

--- scripts/missile-silo.js
+++ scripts/missile-silo.js
@@ -1,8 +1,10 @@
-const silo = extendContent(Turret, "missileSilo", {
+const silo = extendContent(ItemTurret, "missileSilo", {
   load(){
     this.region = Core.atlas.find(this.name);
     this.baseRegion = Core.atlas.find(this.name + "-base");
   }, 
+  draw(tile){
   Draw.rect(this.region,tile.drawx(),tile.drawy());
   Draw.rect(this.baseRegion,tile.drawx(),tile.drawy());
+  } 
 });
