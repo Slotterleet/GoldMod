5d9e09ba931a425a03011b6bc3dee872cad7819c
TheSlaylord <abevnyaev@gmail.com>
2020-09-14 20:08:27 +0300

    Better look of Zuila (definitely not Serpulo-like)

--- scripts/campaign.js
+++ scripts/campaign.js
@@ -1,5 +1,49 @@
+const zuilagen = extend(SerpuloPlanetGenerator, {});
+zuilagen.arr = [
+  [Blocks.water, Blocks.sandWater, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.sandWater, Blocks.stone, Blocks.stone],
+  
+  [Blocks.water, Blocks.sandWater, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.sandWater, Blocks.stone, Blocks.stone, Blocks.stone],
+  
+  [Blocks.water, Blocks.sandWater, Blocks.sand, Blocks.sand, Blocks.salt, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.sandWater, Blocks.stone, Blocks.stone, Blocks.stone],
+  
+  [Blocks.water, Blocks.sandWater, Blocks.sand, Blocks.salt, Blocks.salt, Blocks.salt, Blocks.sand, Blocks.stone, Blocks.stone, Blocks.stone, Blocks.snow, Blocks.iceSnow, Blocks.ice],
+  
+  [Blocks.deepwater, Blocks.water, Blocks.sandWater, Blocks.sand, Blocks.salt, Blocks.sand, Blocks.sand, Blocks.basalt, Blocks.snow, Blocks.snow, Blocks.snow, Blocks.snow, Blocks.ice],
+  
+  [Blocks.deepwater, Blocks.water, Blocks.sandWater, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.dirt, Blocks.iceSnow, Blocks.snow, Blocks.snow, Blocks.ice, Blocks.snow, Blocks.ice],
+  
+  [Blocks.deepwater, Blocks.sandWater, Blocks.sand, Blocks.sand, Blocks.dirt, Blocks.dirt, Blocks.snow, Blocks.snow, Blocks.snow, Blocks.ice, Blocks.ice, Blocks.snow, Blocks.ice],
+  
+  [Blocks.water, Blocks.sandWater, Blocks.sand, Blocks.sand, Blocks.basalt, Blocks.dirt, Blocks.snow, Blocks.snow, Blocks.snow, Blocks.ice, Blocks.snow, Blocks.ice, Blocks.ice],
+  
+  [Blocks.sandWater, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.dirt, Blocks.grass, Blocks.snow, Blocks.snow, Blocks.snow, Blocks.snow, Blocks.snow, Blocks.ice, Blocks.ice],
+  
+  [Blocks.sandWater, Blocks.sand, Blocks.sand, Blocks.grass, Blocks.ice, Blocks.ice, Blocks.snow, Blocks.snow, Blocks.snow, Blocks.snow, Blocks.ice, Blocks.ice, Blocks.ice],
+  
+  [Blocks.water, Blocks.sandWater, Blocks.sand, Blocks.grass, Blocks.grass, Blocks.ice, Blocks.ice, Blocks.snow, Blocks.snow, Blocks.ice, Blocks.ice, Blocks.ice, Blocks.ice],
+  
+  [Blocks.sandWater, Blocks.sandWater, Blocks.sand, Blocks.grass, Blocks.dirt, Blocks.grass, Blocks.iceSnow, Blocks.snow, Blocks.ice, Blocks.ice, Blocks.ice, Blocks.ice, Blocks.ice],
+  
+  [Blocks.sandWater, Blocks.sand, Blocks.snow, Blocks.ice, Blocks.iceSnow, Blocks.snow, Blocks.snow, Blocks.snow, Blocks.ice, Blocks.ice, Blocks.ice, Blocks.ice, Blocks.ice]
+  ];
+zuilagen.scl = 4;
+zuilagen.waterOffset = 0.07;
+zuilagen.basegen = new BaseGenerator();
+zuilagen.water = 2 / zuilagen.arr[0].length;
+zuilagen.dec = [
+  Blocks.grass, Blocks.dirt,
+  Blocks.grass, Blocks.dirt,
+  Blocks.water, Blocks.water,
+  Blocks.darksandWater, Blocks.darksandWater
+];
+zuilagen.tars = [
+  Blocks.grass, Blocks.shale,
+  Blocks.dirt, Blocks.shale
+  ];
+
 const zuila = new JavaAdapter(Planet, {}, "zuila", Planets.sun, 3, 1.5);
 
-zuila.sectors = 120;
+zuila.generator = zuilagen;
 zuila.startSector = 27;
 zuila.atmosphereColor = Color.valueOf("deae66");
+zuila.meshLoader = () => new HexMesh(zuila, 6);
