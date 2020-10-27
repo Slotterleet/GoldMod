ef905aaf0d94801b25d9cab54e8d2818b1c9d0aa
TheSlaylord <abevnyaev@gmail.com>
2020-10-27 19:26:00 +0300

    temporarily archive the planets

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,8 @@ require('missile-silo');
 require('roads');
 require('gForeshadow');
 
-require('campaign/zuila');
+//press F to planets (as for now)
+//require('campaign/zuila');
 require('campaign/serpulo');
-require('campaign/cori');
+//require('campaign/cori');
 require('campaign/techtree');
