1649f6e040a113b8e3c2ff01179e408873a053bf
TheSlaylord <abevnyaev@gmail.com>
2020-11-06 18:00:07 +0300

    Bugfixes 2 electric boogaloo

--- scripts/campaign/techtree.js
+++ scripts/campaign/techtree.js
@@ -299,5 +299,49 @@ const gNova = extendContent(UnitType, "goldenNova", {
   }
 });
 gNova.constructor = () => extend(BuilderMechMinerUnit, {});
-const nova_abil = new JavaAdapter(RepairFieldAbility, {}, 5, 180, 60);
+const nova_abil = new JavaAdapter(RepairFieldAbility, {
+  load(){
+    this.localizedName = Core.bundle.get("ability.repairfield");
+  }
+}, 5, 180, 60);
 gNova.abilities.add(nova_abil);
+
+const gPulsar = extendContent(UnitType, "goldenPulsar", {
+  researchRequirements(){
+    return ItemStack.with(Items.silicon, 1500, Items.graphite, 1500, Vars.content.getByName(ContentType.item, "goldmod-goldIngot"), 750)
+  }
+});
+gPulsar.constructor = () => extend(BuilderMechMinerUnit, {});
+const pulsar_abil = new JavaAdapter(ShieldRegenFieldAbility, {
+  load(){
+    this.localizedName = Core.bundle.get("ability.shieldregenfield");
+  }
+}, 50, 100, 300, 60);
+gPulsar.abilities.add(pulsar_abil);
+const gp_weapon = extendContent(Weapon, "gHealShotgun", {});
+gp_weapon.top = false;
+gp_weapon.x = 5;
+gp_weapon.y = 0.5;
+gp_weapon.shake = 2.2;
+gp_weapon.shootY = 2.5;
+gp_weapon.reload = 38;
+gp_weapon.shots = 3;
+gp_weapon.inaccuracy = 30;
+gp_weapon.shotDelay = 0.5;
+gp_weapon.spacing = 0;
+gp_weapon.ejectEffect = Fx.none;
+gp_weapon.recoil = 2.5;
+gp_weapon.shootSound = Sounds.pew;
+const gp_bullet = extend(LightningBulletType, {});
+gp_bullet.lightningColor = gp_bullet.hitColor = Color.valueOf("98ffa9");
+gp_bullet.damage = 45;
+const lightning = new JavaAdapter(BulletType, {}, 0.0001, 0);
+lightning.collidesTeam = true;
+lightning.status = StatusEffects.shocked;
+lightning.statusDuration = 10;
+lightning.hittable = false;
+lightning.healPercent = 6;
+lightning.despawnEffect = Fx.none;
+gp_bullet.lightningType = lightning;
+gp_weapon.bullet = gp_bullet;
+gPulsar.weapons.add(gp_weapon);
